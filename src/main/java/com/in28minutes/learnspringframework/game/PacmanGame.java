c1fe986bf21e17b5d9a3f23047710fd055381a40
Daryn <dhaffer1991@gmail.com>
2022-12-27 09:23:04 -0600

    1-14 Complete

---
+++ src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,21 @@
+package com.in28minutes.learnspringframework.game;
+
+public class PacmanGame implements GamingConsole {
+	
+	public void up() {
+		System.out.println("Nom Up");
+	}
+	
+	public void down() {
+		System.out.println("Nom Down");
+	}
+	
+	public void left() {
+		System.out.println("Nom Left");
+	}
+	
+	public void right() {
+		System.out.println("Nom Right");
+	}
+	
+}
